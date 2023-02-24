68689c3c0cc598067236f1f49b7cc75718eda9ab
Yong Park <pyongho0306@gmail.com>
2023-02-24 09:49:42 -0800

    Stub up model for post

---
+++ models/post.js
@@ -0,0 +1,35 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Post.init(
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      date: DataTypes.DATE,
+      profileId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        onDelete: "CASCADE",
+        references: {
+          model: "Profiles",
+          key: "id",
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Post",
+    }
+  );
+  return Post;
+};
