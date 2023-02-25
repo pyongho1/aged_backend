9a0c567601d6c1f424226fe61201998230165b71
Yong Park <pyongho0306@gmail.com>
2023-02-25 13:25:37 -0800

    Stub up update

--- controllers/posts.js
+++ controllers/posts.js
@@ -37,9 +37,21 @@ const deletePost = async (req, res) => {
   }
 };
 
+const update = async (req, res) => {
+  try {
+    const post = await Post.findByPk(req.params.id);
+    post.set(req.body);
+    await post.save();
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
 module.exports = {
   create,
   index,
   show,
   delete: deletePost,
+  update,
 };
--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,7 @@ const postsCtrl = require("../controllers/posts.js");
 router.get("/", postsCtrl.index);
 router.get("/:id", postsCtrl.show);
 router.post("/", postsCtrl.create);
+router.put("/:id", postsCtrl.update);
 router.delete("/:id", postsCtrl.delete);
 
 module.exports = router;
